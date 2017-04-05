36a85d1a76f38832e7de0c8235d858a8efb0eae3
JGuiraud <jerome31guiraud@gmail.com>
2017-04-05 17:32:43 +0200

    new version

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    var stickyNavTop = $('.menuGen').offset().top;
+
+    var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > stickyNavTop) {
+            $('.menuGen').addClass('sticky');
+            $(".navbar").addClass('noshadow');
+        } else {
+            $('.menuGen').removeClass('sticky');
+            $(".navbar").removeClass('noshadow')
+        }
+    };
+    stickyNav();
+
+    $(window).scroll(function () {
+        stickyNav();
+    });
+
+    /* konami code */
+    var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        n = 0;
+    $(document).keydown(function (e) {
+        if (e.keyCode === k[n++]) {
+            if (n === k.length) {
+                $('body').toggleClass("rotate")
+                n = 0;
+                return false;
+            }
+        } else {
+            n = 0;
+        }
+    });
+
+    var colours = ["#003333"];
+
+    var competences = ["Html5", "CSS3", "Javascript", "jQuery", "Ajax", "NodeJs", "PHP", "Git", "ReactJs", "Bootstrap", "Photoshop", "inDesign", "Illustrator", "PremierePro"]
+
+
+    for (var i = 0; i < competences.length; i++) {
+        var num = Math.floor(Math.random() * colours.length);
+        $("#cptces").append("<p class='competence " + competences[i] + "'>" + competences[i] + "</p>");
+        $('.' + competences[i]).css("background-color", colours[num]);
+    }
+
+
+})
